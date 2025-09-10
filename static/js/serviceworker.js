bd74da48f273060905fda76b736d7ac53697ddc3
vidhyanandreddy <vidhyanand067@gmail.com>
2025-09-10 23:00:06 +0530

    New Navbar

--- static/js/serviceworker.js
+++ static/js/serviceworker.js
@@ -18,3 +18,42 @@ self.addEventListener("fetch", function (event) {
     })
   );
 });
+
+const CACHE_NAME = "soundory-cache-v1";
+const urlsToCache = [
+  "/",
+  "/static/css/styles.css",
+  "/static/js/main.js",
+  // add other resources you want cached
+];
+
+// Install event - cache all files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activate event - cleanup old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
+
+// Fetch event - respond with cached or fetch from network
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
